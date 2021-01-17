b28c7a31cb53c4a1d3223a002384e45df6e58fe0
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2021-01-17 23:09:27 +0100

    feat:viewsets

--- marketgenius/marketapi/views.py
+++ marketgenius/marketapi/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import ProductSerializer, TicketSerializer
+from .models import Product, Ticket
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        product = Product.objects.all()
+        return product
+
+class TicketViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketSerializer
+
+    def get_queryset(self):
+        ticket = Ticket.objects.all()
+        return ticket     
