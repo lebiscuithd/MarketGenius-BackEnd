415009bfe54aab4561b70b341a3c6e7bc6b4e649
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2021-01-17 22:47:09 +0100

    feat:add-models

--- marketgenius/marketapi/models.py
+++ marketgenius/marketapi/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_price = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
+
+class Ticket(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.FloatField()
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.id
+    
+
